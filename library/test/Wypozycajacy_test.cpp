a6fc75bf35497f5607f9e1422f53c0c5beb89655
247722 Luzak Mateusz <247722@edu.p.lodz.pl>
2023-06-16 20:21:29 +0200

    Dodanie exceptions test Wypozycajacy_test.cpp

--- library/test/Wypozycajacy_test.cpp
+++ library/test/Wypozycajacy_test.cpp
@@ -21,26 +21,22 @@ BOOST_AUTO_TEST_SUITE(WypozycajacySuiteCorrect)
         Wypozycajacy w3(pozostali, "Piotr Zieliński", testDate, "Wrocław");
 
         BOOST_CHECK_EQUAL(w1.getTypWypozycajacy()->getMaksLKsiazek(), 10);
-        BOOST_CHECK_EQUAL(w2.getNazwa(), "Anna Nowak");
-        BOOST_CHECK_EQUAL(w3.getAdres(), "Wrocław");
+
         w1.setNazwa("Marian Las");
         w1.getTypWypozycajacy()->setMaxDlWypoz(5);
         BOOST_CHECK_EQUAL(w1.getNazwa(),"Marian Las");
         BOOST_CHECK_EQUAL( w1.getTypWypozycajacy()->getMaxDlWypoz(),5);
     }
-
-    BOOST_AUTO_TEST_CASE(Info_Wyp_Test_asd)
+    BOOST_AUTO_TEST_CASE(SetterExceptionTest)
     {
         PTime testDate = boost::posix_time::time_from_string("2023-05-30 00:00:00");
         Typ_Wypozycajacy_Ptr uczen = std::make_shared<Uczen>(0.0, 30, 10, "2");
-        Wypozycajacy w3(uczen, "Jan Kowalski", testDate, "Kraków");
+        Wypozycajacy wypozycajacy(uczen, "Jan Kowalski", testDate, "Kraków");
 
-        std::string expected_info = "Nazwa: Jan Kowalski\n"
-                                    "dataUr: 2023-May-30 00:00:00\n"
-                                    "adres: Kraków\n"
-                                    "Typ_wypo: " + uczen->Typ_Info();
-
-        //BOOST_CHECK_EQUAL(w3.pobierz_inforacje_wyp(), expected_info);
+        BOOST_CHECK_THROW(wypozycajacy.setTypWypozycajacy(nullptr), WypozyczajacyException);
+        BOOST_CHECK_THROW(wypozycajacy.setNazwa(""), WypozyczajacyException);
+        BOOST_CHECK_THROW(wypozycajacy.setAdres(""), WypozyczajacyException);
     }
+
 
 BOOST_AUTO_TEST_SUITE_END()
