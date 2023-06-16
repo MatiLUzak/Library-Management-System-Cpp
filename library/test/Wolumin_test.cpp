be032fee6aa93cab0b066facb649cfcff072edb4
247722 Luzak Mateusz <247722@edu.p.lodz.pl>
2023-06-16 18:31:03 +0200

    exceptions Wolumin tests Beletrystyka i Czasopismo

--- library/test/Wolumin_test.cpp
+++ library/test/Wolumin_test.cpp
@@ -59,6 +59,19 @@ BOOST_AUTO_TEST_SUITE(WoluminSuiteCorrect)
         BOOST_CHECK_EQUAL(c1.getTytul(), "Tytuł2");
         BOOST_CHECK_EQUAL(c1.getNrWydania(), "Nr2");
     }
+    BOOST_AUTO_TEST_CASE(CzasopismoExceptionCase) {
+        BOOST_CHECK_THROW(Czasopismo("", "Polski", "Tytuł1", "Nr1"), WoluminException);
+        BOOST_CHECK_THROW(Czasopismo("Wydawnictwo1", "", "Tytuł1", "Nr1"), WoluminException);
+        BOOST_CHECK_THROW(Czasopismo("Wydawnictwo1", "Polski", "", "Nr1"), WoluminException);
+        BOOST_CHECK_THROW(Czasopismo("Wydawnictwo1", "Polski", "Tytuł1", ""), WoluminException);
+
+        Czasopismo c1("Wydawnictwo1", "Polski", "Tytuł1", "Nr1");
+
+        BOOST_CHECK_THROW(c1.setWydawnictwo(""), WoluminException);
+        BOOST_CHECK_THROW(c1.setJezyk(""), WoluminException);
+        BOOST_CHECK_THROW(c1.setTytul(""), WoluminException);
+        BOOST_CHECK_THROW(c1.setNrWydania(""), WoluminException);
+    }
     BOOST_AUTO_TEST_CASE(BeletrystykaCase) {
         std::vector<std::string> autors = {"Autor1", "Autor2"};
         Beletrystyka b1("Wydawnictwo1", "Polski", "Tytuł1", autors, "Przedział1", "Rodzaj1");
@@ -78,5 +91,24 @@ BOOST_AUTO_TEST_SUITE(WoluminSuiteCorrect)
         BOOST_CHECK_EQUAL(b1.getRodzaj(), "Rodzaj2");
         BOOST_CHECK(b1.getAutor() == newAutors);
     }
+    BOOST_AUTO_TEST_CASE(BeletrystykaExceptionCase) {
+        std::vector<std::string> autors = {"Autor1", "Autor2"};
+        std::vector<std::string> emptyAutors;
 
+        BOOST_CHECK_THROW(Beletrystyka("", "Polski", "Tytuł1", autors, "Przedział1", "Rodzaj1"), WoluminException);
+        BOOST_CHECK_THROW(Beletrystyka("Wydawnictwo1", "", "Tytuł1", autors, "Przedział1", "Rodzaj1"), WoluminException);
+        BOOST_CHECK_THROW(Beletrystyka("Wydawnictwo1", "Polski", "", autors, "Przedział1", "Rodzaj1"), WoluminException);
+        BOOST_CHECK_THROW(Beletrystyka("Wydawnictwo1", "Polski", "Tytuł1", autors, "", "Rodzaj1"), WoluminException);
+        BOOST_CHECK_THROW(Beletrystyka("Wydawnictwo1", "Polski", "Tytuł1", autors, "Przedział1", ""), WoluminException);
+        BOOST_CHECK_THROW(Beletrystyka("Wydawnictwo1", "Polski", "Tytuł1", emptyAutors, "Przedział1", "Rodzaj1"), WoluminException);
+
+        Beletrystyka b1("Wydawnictwo1", "Polski", "Tytuł1", autors, "Przedział1", "Rodzaj1");
+
+        BOOST_CHECK_THROW(b1.setWydawnictwo(""), WoluminException);
+        BOOST_CHECK_THROW(b1.setJezyk(""), WoluminException);
+        BOOST_CHECK_THROW(b1.setTytul(""), WoluminException);
+        BOOST_CHECK_THROW(b1.setPrzedziałWiekowy(""), WoluminException);
+        BOOST_CHECK_THROW(b1.setRodzaj(""), WoluminException);
+        BOOST_CHECK_THROW(b1.setAutor(emptyAutors), WoluminException);
+    }
 BOOST_AUTO_TEST_SUITE_END()
