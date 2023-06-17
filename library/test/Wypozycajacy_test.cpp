98c2539dd767ada3792d325ed2d134acc3f2d16c
247722 Luzak Mateusz <247722@edu.p.lodz.pl>
2023-06-17 10:05:29 +0200

    Uwydajnienie Wypozycajacy_test.cpp

--- library/test/Wypozycajacy_test.cpp
+++ library/test/Wypozycajacy_test.cpp
@@ -13,12 +13,8 @@ BOOST_AUTO_TEST_SUITE(WypozycajacySuiteCorrect)
     {
         PTime testDate = boost::posix_time::time_from_string("2023-05-30 00:00:00");
         Typ_Wypozycajacy_Ptr uczen = std::make_shared<Uczen>(0.0, 30, 10, "2");
-        Typ_Wypozycajacy_Ptr nauczyciel = std::make_shared<Nauczyciel>(0.0, 30, 10, "Dr");
-        Typ_Wypozycajacy_Ptr pozostali = std::make_shared<Pozostali>(0.0, 30, 10, "Inżynier");
 
         Wypozycajacy w1(uczen, "Jan Kowalski", testDate, "Kraków");
-        Wypozycajacy w2(nauczyciel, "Anna Nowak", testDate, "Warszawa");
-        Wypozycajacy w3(pozostali, "Piotr Zieliński", testDate, "Wrocław");
 
         BOOST_CHECK_EQUAL(w1.getTypWypozycajacy()->getMaksLKsiazek(), 10);
 
@@ -26,6 +22,8 @@ BOOST_AUTO_TEST_SUITE(WypozycajacySuiteCorrect)
         w1.getTypWypozycajacy()->setMaxDlWypoz(5);
         BOOST_CHECK_EQUAL(w1.getNazwa(),"Marian Las");
         BOOST_CHECK_EQUAL( w1.getTypWypozycajacy()->getMaxDlWypoz(),5);
+        BOOST_CHECK(!w1.getUuid().is_nil());
+        BOOST_CHECK_EQUAL(w1.getDataUr(), testDate);
     }
     BOOST_AUTO_TEST_CASE(SetterExceptionTest)
     {
